90e350c0bdef236d70d1f22fae5358bca007db47
orxfun <orx.ugur.arikan@gmail.com>
2025-10-24 12:21:54 +0200

    Queue documentation

--- src/queue.rs
+++ src/queue.rs
@@ -1,6 +1,21 @@
 use orx_concurrent_queue::{ConcurrentQueue, DefaultConPinnedVec};
 use orx_pinned_vec::ConcurrentPinnedVec;
 
+/// A queue of elements that will be returned by the [`ConcurrentRecursiveIter`].
+///
+/// Note that the concurrent recursive iterator is built on top of the [`ConcurrentQueue`].
+/// `Queue` is just a wrapper over the concurrent queue, exposing only two methods:
+///
+/// * `push` to add one task at a time,
+/// * `extend` to add multiple tasks with known length together with a single update.
+///
+/// Notice that `push` is very flexible and makes the pushed element available for iteration
+/// as fast as possible.
+///
+/// On the other hand, `extend` minimizes the overhead of concurrency.
+///
+/// [`ConcurrentRecursiveIter`]: crate::ConcurrentRecursiveIter
+/// [`ConcurrentQueue`]: orx_concurrent_queue::ConcurrentQueue
 pub struct Queue<'a, T, P = DefaultConPinnedVec<T>>
 where
     T: Send,
@@ -14,11 +29,16 @@ where
     T: Send,
     P: ConcurrentPinnedVec<T>,
 {
+    /// Pushes the `element` to the iterator, making it available to all threads as fast as possible.
     #[inline(always)]
     pub fn push(&self, element: T) {
         self.queue.push(element);
     }
 
+    /// Pushes all `elements` to the iterator with a single update on the concurrent state.
+    ///
+    /// All of the elements will be available to all threads at the same time, once writing all of them
+    /// to the queue is completed.
     #[inline(always)]
     pub fn extend<I>(&self, elements: I)
     where
