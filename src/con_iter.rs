775e3b1076aeeadd81179726590b4ead77ed42de
orxfun <orx.ugur.arikan@gmail.com>
2025-10-24 10:32:07 +0200

    init con-iter implementation

--- src/con_iter.rs
+++ src/con_iter.rs
@@ -1,15 +1,60 @@
+use crate::queue::Queue;
+use orx_concurrent_iter::ConcurrentIter;
 use orx_concurrent_queue::{ConcurrentQueue, DefaultConPinnedVec};
 use orx_pinned_vec::{ConcurrentPinnedVec, IntoConcurrentPinnedVec};
-
-use crate::queue::Queue;
 
 pub struct ConcurrentRecursiveIter<T, E, P = DefaultConPinnedVec<T>>
 where
     T: Send,
     P: ConcurrentPinnedVec<T>,
     <P as ConcurrentPinnedVec<T>>::P: IntoConcurrentPinnedVec<T, ConPinnedVec = P>,
-    E: Fn(&T, &Queue<T, P>),
+    E: Fn(&T, &Queue<T, P>) + Sync,
 {
     queue: ConcurrentQueue<T, P>,
     extend: E,
+}
+
+impl<T, E, P> ConcurrentIter for ConcurrentRecursiveIter<T, E, P>
+where
+    T: Send,
+    P: ConcurrentPinnedVec<T>,
+    <P as ConcurrentPinnedVec<T>>::P: IntoConcurrentPinnedVec<T, ConPinnedVec = P>,
+    E: Fn(&T, &Queue<T, P>) + Sync,
+{
+    type Item = T;
+
+    type SequentialIter = core::iter::Empty<T>;
+
+    type ChunkPuller<'i>
+        = todo!()
+    where
+        Self: 'i;
+
+    fn into_seq_iter(self) -> Self::SequentialIter {
+        todo!()
+    }
+
+    fn skip_to_end(&self) {
+        todo!()
+    }
+
+    fn next(&self) -> Option<Self::Item> {
+        todo!()
+    }
+
+    fn next_with_idx(&self) -> Option<(usize, Self::Item)> {
+        todo!()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        todo!()
+    }
+
+    fn is_completed_when_none_returned(&self) -> bool {
+        todo!()
+    }
+
+    fn chunk_puller(&self, chunk_size: usize) -> Self::ChunkPuller<'_> {
+        todo!()
+    }
 }
