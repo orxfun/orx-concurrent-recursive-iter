9281687df847500892c64206dad70fdf774484e2
orxfun <orx.ugur.arikan@gmail.com>
2025-10-24 11:25:45 +0200

    export iter types

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,9 @@ mod queue;
 
 // re-import
 pub use orx_concurrent_iter::*;
+
+pub use chunk::DynChunk;
+pub use chunk_puller::DynChunkPuller;
+pub use con_iter::ConcurrentRecursiveIter;
+pub use dyn_seq_queue::DynSeqQueue;
+pub use queue::Queue;
