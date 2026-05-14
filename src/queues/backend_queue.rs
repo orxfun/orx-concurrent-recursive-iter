20e40eb8947ba305ec054a6b9a8ac337b0685f49
orxfun <orx.ugur.arikan@gmail.com>
2026-05-14 20:33:48 +0200

    wip

--- src/queues/backend_queue.rs
+++ src/queues/backend_queue.rs
@@ -1,22 +1,24 @@
 #[doc(hidden)]
-pub trait BackendQueue<T>: Send + Sync
+pub trait BackendQueue<T>: Sync
 where
     T: Send,
 {
-    type PullIter: ExactSizeIterator<Item = T>;
+    type PullIter<'a>: ExactSizeIterator<Item = T>
+    where
+        Self: 'a;
 
-    fn push(&self, element: T);
+    fn push(&self, value: T);
 
-    fn extend<I, Iter>(&self, elements: I)
+    fn extend<I, Iter>(&self, values: I)
     where
         I: IntoIterator<Item = T, IntoIter = Iter>,
         Iter: ExactSizeIterator<Item = T>;
 
     fn pop(&self) -> Option<T>;
 
-    fn pull(&self, chunk_size: usize) -> Option<Self::PullIter>;
+    fn pull(&self, chunk_size: usize) -> Option<Self::PullIter<'_>>;
 
-    fn pull_with_idx(&self, chunk_size: usize) -> Option<(usize, Self::PullIter)>;
+    fn pull_with_idx(&self, chunk_size: usize) -> Option<(usize, Self::PullIter<'_>)>;
 
     fn next_yield_idx(&self) -> usize;
 
--- src/queues/mod.rs
+++ src/queues/mod.rs
@@ -1,1 +1,2 @@
 mod backend_queue;
+mod concurrent_queue;
